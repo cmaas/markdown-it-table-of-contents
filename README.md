a746627154c408bf31f47fd695adfb1ea59c5354
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2015-05-18 16:38:21 +0200

    Updates README

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # markdown-it-table-of-contents
-A table of contents plugin for Markdown-it 
+A table of contents plugin for Markdown-it. Based on https://github.com/samchrisinger/markdown-it-toc but 
+simpler, a bit more customizable and with a default slugifier that matches that of https://www.npmjs.com/package/markdown-it-anchor.
+
+## Usage
+
+``` javascript
+var MarkdownIt = require("markdown-it");
+var md = new MarkdownIt();
+
+md.use(require("markdown-it-anchor")); // Optional, but makes sense as you really want to link to something
+md.use(require("markdown-it-table-of-contents"));
+```
+
+Then add `[[toc]]` where you want the table of contents to be added in your markdown.
+
+## Example markdown
+
+This markdown:
+``` markdown
+# Heading
+
+[[toc]]
+
+## Sub heading 1
+Some nice text
+
+## Sub heading 2
+Some even nicer text
+```
+
+... would render this HTML using the default options specified in "usage" above:
+``` html
+<h1 id="heading">Heading</h1>
+
+<div class="table-of-contents">
+  <ul>
+    <li><a href="#sub-heading-1">Sub heading 1</a></li>
+    <li><a href="#sub-heading-2">Sub heading 2</a></li>
+  </ul>
+</div>
+
+<h2 id="sub-heading-1">Sub heading 1</h2>
+<p>Some nice text</p>
+
+<h2 id="sub-heading-2">Sub heading 2</h2>
+<p>Some even nicer text</p>
+```
+
+## Options
+
+You may specify options when `use`ing the plugin. like so:
+``` javascript
+md.use(require("markdown-it-table-of-contents", options));
+```
+
+These options are available:
+
+Name              | Description                               | Default
+------------------|-------------------------------------------|------------------------------------
+"includeLevel"    | The heading level to use (2 for h2:s etc) | 2
+"containerClass"  | The class for the container DIV           | "table-of-contents"
+"slugify"         | A custom slugification function           | [string.js' `slugify`][slugify]
+
+[slugify]: http://stringjs.com/#methods/slugify
---
+++ index.js
@@ -0,0 +1,90 @@
+"use strict";
+var string = require("string");
+var assign = require("lodash.assign");
+var defaults = {
+  includeLevel: 2,
+  containerClass: "table-of-contents",
+  slugify: function(str) {
+    return string(str).slugify().toString();
+  }
+};
+
+module.exports = function(md, options) {
+  var options = assign({}, defaults, options);
+  var tocRegexp = /^\[\[toc\]\]/im;
+  var gstate;
+
+  function toc(state, silent) {
+    var token;
+    var match;
+
+    while (state.src.indexOf("\n") >= 0 && state.src.indexOf("\n") < state.src.indexOf("[[toc]]")) {
+      if (state.tokens.slice(-1)[0].type === "softbreak") {
+        state.src = state.src.split("\n").slice(1).join("\n");
+        state.pos = 0;
+      }
+    }
+
+    // Reject if the token does not start with [
+    if (state.src.charCodeAt(state.pos) !== 0x5B /* [ */ ) {
+      return false;
+    }
+    // Don't run any pairs in validation mode
+    if (silent) { 
+      return false;
+    }
+
+    // Detect TOC markdown
+    match = tocRegexp.exec(state.src);
+    match = !match ? [] : match.filter(function(m) { return m; });
+    if (match.length < 1) {
+      return false;
+    }
+
+    // Build content
+    token = state.push("toc_open", "toc", 1);
+    token.markup = "[[toc]]";
+    token = state.push("toc_body", "", 0);
+    token = state.push("toc_close", "toc", -1);
+
+    // Update pos so the parser can continue
+    var newline = state.src.indexOf("\n");
+    if (newline !== -1) {
+      state.pos = state.pos + newline;
+    } else {
+      state.pos = state.pos + state.posMax + 1;
+    }
+
+    return true;
+  }
+
+  md.renderer.rules.toc_open = function(tokens, index) {
+    return "<div class=\"table-of-contents\">";
+  };
+
+  md.renderer.rules.toc_close = function(tokens, index) {
+    return "</div>";
+  };
+
+  md.renderer.rules.toc_body = function(tokens, index) {
+    var headings = [];
+    for (var i = 0, size = gstate.tokens.length; i < size; i++) {
+      var token = gstate.tokens[i];
+      var heading = gstate.tokens[i - 1];
+      if (token.type !== "heading_close" || token.tag.substr(1, 1) != options.includeLevel || heading.type !== "inline") {
+        continue; // Skip if not matching criteria
+      }
+      headings.push("<li><a href=\"#\"" + options.slugify(heading.content) + "\">" + heading.content + "</a></li>");
+    }
+
+    return "<ul>" + headings.join("") + "</ul>";
+  };
+
+  // Catch all the tokens for iteration later
+  md.core.ruler.push("grab_state", function(state) {
+    gstate = state;
+  });
+
+  // Insert TOC
+  md.inline.ruler.after("emphasis", "toc", toc);
+};
